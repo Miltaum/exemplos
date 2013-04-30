b49f13d63514610f31b6d993447c86834c9ec75f
Milton Sérgio de Oliveira Junior <msojunior@gmail.com>
2013-04-30 18:10:55 -0300

    Entrada de programas de exemplos
    EXEMPLOS

---
+++ MJr-MessageBox/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.txtCaption = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.cmbIcone = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Image = ((System.Drawing.Image)(resources.GetObject("btnEnviar.Image")));
+            this.btnEnviar.Location = new System.Drawing.Point(354, 196);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(106, 57);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(77, 90);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(354, 20);
+            this.txtMensagem.TabIndex = 1;
+            // 
+            // txtCaption
+            // 
+            this.txtCaption.Location = new System.Drawing.Point(77, 58);
+            this.txtCaption.Name = "txtCaption";
+            this.txtCaption.Size = new System.Drawing.Size(354, 20);
+            this.txtCaption.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mensagem";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Caption";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Location = new System.Drawing.Point(77, 130);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(142, 21);
+            this.cmbTipo.TabIndex = 5;
+            // 
+            // cmbIcone
+            // 
+            this.cmbIcone.FormattingEnabled = true;
+            this.cmbIcone.Location = new System.Drawing.Point(270, 130);
+            this.cmbIcone.Name = "cmbIcone";
+            this.cmbIcone.Size = new System.Drawing.Size(161, 21);
+            this.cmbIcone.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tipo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(225, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Icones";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 265);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbIcone);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCaption);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.btnEnviar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MJr - Message Box...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.TextBox txtCaption;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.ComboBox cmbIcone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MJr-MessageBox/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarCombos();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtMensagem.Text, txtCaption.Text, TipoButtons(cmbTipo.Text), TipoIcons(cmbIcone.Text));
+        }
+
+        private void CarregarCombos()
+        {
+            cmbTipo.Items.Clear();
+            cmbTipo.Items.Add(MessageBoxButtons.AbortRetryIgnore.ToString());
+            cmbTipo.Items.Add(MessageBoxButtons.OK.ToString());
+            cmbTipo.Items.Add(MessageBoxButtons.OKCancel.ToString());
+            cmbTipo.Items.Add(MessageBoxButtons.RetryCancel.ToString());
+            cmbTipo.Items.Add(MessageBoxButtons.YesNo.ToString());
+            cmbTipo.Items.Add(MessageBoxButtons.YesNoCancel.ToString());
+
+            cmbIcone.Items.Clear();
+            cmbIcone.Items.Add(MessageBoxIcon.Asterisk.ToString());
+            cmbIcone.Items.Add(MessageBoxIcon.Error.ToString());
+            cmbIcone.Items.Add(MessageBoxIcon.Exclamation.ToString());
+            cmbIcone.Items.Add(MessageBoxIcon.Hand.ToString());
+            cmbIcone.Items.Add(MessageBoxIcon.Information.ToString());
+            cmbIcone.Items.Add(MessageBoxIcon.None.ToString());
+            cmbIcone.Items.Add(MessageBoxIcon.Question.ToString());
+            cmbIcone.Items.Add(MessageBoxIcon.Stop.ToString());
+            cmbIcone.Items.Add(MessageBoxIcon.Warning.ToString());
+        }
+
+        private MessageBoxButtons TipoButtons(string tipo)
+        {
+            int ret=0;
+
+            switch (tipo)
+            {
+                case "AbortRetryIgnore":
+                    ret = (int)MessageBoxButtons.AbortRetryIgnore;
+                    break;
+                case "OK":
+                    ret = (int)MessageBoxButtons.OK;
+                    break;
+                case "OKCancel":
+                    ret = (int)MessageBoxButtons.OKCancel;
+                    break;
+                case "RetryCancel":
+                    ret = (int)MessageBoxButtons.RetryCancel;
+                    break;
+                case "YesNo":
+                    ret = (int)MessageBoxButtons.YesNo;
+                    break;
+                case "YesNoCancel":
+                    ret = (int)MessageBoxButtons.YesNoCancel;
+                    break;
+            }
+
+            return (MessageBoxButtons)ret;
+        }
+
+        private MessageBoxIcon TipoIcons(string tipo)
+        {
+            int ret = 0;
+
+            switch (tipo)
+            {
+                case "Asterisk":
+                    ret = (int)MessageBoxIcon.Asterisk;
+                    break;
+                case "OK":
+                    ret = (int)MessageBoxIcon.Error;
+                    break;
+                case "Exclamation":
+                    ret = (int)MessageBoxIcon.Exclamation;
+                    break;
+                case "Hand":
+                    ret = (int)MessageBoxIcon.Hand;
+                    break;
+                case "Information":
+                    ret = (int)MessageBoxIcon.Information;
+                    break;
+                case "None":
+                    ret = (int)MessageBoxIcon.None;
+                    break;
+                case "Question":
+                    ret = (int)MessageBoxIcon.Question;
+                    break;
+                case "Stop":
+                    ret = (int)MessageBoxIcon.Stop;
+                    break;
+                case "Warning":
+                    ret = (int)MessageBoxIcon.Warning;
+                    break;
+            }
+
+            return (MessageBoxIcon)ret;
+        }
+
+    }
+}
